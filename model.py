c5e9a1685a9d6b15eaa027920fdb07c37cbfb491
Nadya <42322648+dsashulya@users.noreply.github.com>
2019-06-05 14:46:32 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,12 +4,21 @@ from scipy import sparse
 from sklearn.cluster import KMeans
 import pickle 
 
-edamtx = pd.read_csv('eda_log.csv')
-edamtx.drop(columns='Unnamed: 0', inplace=True)
-edamtx = sparse.csr_matrix(edamtx)
+eda = pd.read_csv('eda_log.csv')
+eda.drop(columns='Unnamed: 0', inplace=True)
+
+np.save('columns.npy', eda.columns)
+
+eda = sparse.csr_matrix(eda)
 
 kmeans = KMeans(n_clusters=50)
-kmeans.fit_predict(edamtx)
+clusters = kmeans.fit_predict(eda)
+
+eda = pd.read_csv('eda_total.csv')
+
+eda['clusters'] = list(clusters)
+
+eda.to_csv('eda_total.csv')
 
 with open('model.pkl', 'wb') as file:
     pickle.dump(kmeans, file)
